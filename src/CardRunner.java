e37dfd2c82aabce4fd21f597606c61e519b07233
StuartRucker <Stuart.a.rucker@gmail.com>
2014-11-04 06:59:23 -0500

    Update CardRunner.java

--- src/CardRunner.java
+++ src/CardRunner.java
@@ -4,12 +4,17 @@
 public class CardRunner
 {
     public static void main(String[] args){
-        CardCard JackofSpades = new CardCard(3,5);
-        CardCard otherCard = new CardCard(2,4);
+        
+    
+        
+        CardDeck deck = new CardDeck();
+        deck.shuffle();
+        
         CardHand Player1Hand = new CardHand();
-        Player1Hand.addCard(JackofSpades);
-        Player1Hand.addCard(otherCard);
+        Player1Hand.addCard( deck.removeCard());
+        Player1Hand.addCard(deck.removeCard());
         CardDisplay player1Display = new CardDisplay(Player1Hand, "Player 1");
         player1Display.makeDisplay();
+  
     }
 }
