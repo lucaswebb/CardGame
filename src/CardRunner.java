bb9bc52fa0621929c2b7e682f806185a7a4bd6be
lucasawebb <lucasawebb@gmail.com>
2014-11-12 11:44:10 -0500

    Comments on Runner, Card, Options

--- src/CardRunner.java
+++ src/CardRunner.java
@@ -5,27 +5,30 @@ public class CardRunner
 {
     public static void main(String[] args){
 
-
+        //Create a deck and shuffle it
         CardDeck deck = new CardDeck();
         deck.shuffle();
 
+        //Create hands and initialize display
         CardHand Player1Hand = new CardHand(false);//input indicates whether the hand is a dealer
         CardHand Dealer = new CardHand(true);
         CardDisplay player1Display = new CardDisplay(Player1Hand,Dealer, "Player 1");
 
 
-
-
+        //Set money and bet amounts and print welcome message
         player1Display.printBig("welcome to BlackJack");
         int money = 100;
         int betAmount = 0;
 
+        //Start the keep playing loop
+        //Keeps track of whether the player wants to play again
         boolean keepPlaying = true;
         boolean firstTimeThroughLoop = true;
         while(keepPlaying) {
             player1Display.printMedium("Your current money is " + money);
             player1Display.printSmall("Would you like to Play?");
 
+            //Enable slider and reads players option to play or exit
             player1Display.enableSlider();
             int dec;
             if(firstTimeThroughLoop)
@@ -33,20 +36,22 @@ public class CardRunner
             else
                 dec = player1Display.getDecision("play again", "exit", "", "","");
 
-
+            //Deals cards and sets bet then shuffles
             betAmount = player1Display.getBet();
-            //remove cards after input is entered
             deck.addCard(Dealer.removeCards());
             deck.addCard(Player1Hand.removeCards());
             player1Display.refreshHand(Player1Hand,Dealer);
             deck.shuffle();
 
+            //Option to exit
             if (dec == 2) {
                 player1Display.printBig("Exiting game...");
                 player1Display.printSmall("");
                 keepPlaying = false;
 
                 System.out.println("exiting game");
+            //Option to keep playing
+            //Deals more cards and refreshes
             } else {
                 Player1Hand.addCard(deck.removeCard());
                 Player1Hand.addCard(deck.removeCard());
@@ -62,14 +67,18 @@ public class CardRunner
                 boolean playerTurn = true;
                 while (Player1Hand.getSum() < 21 && playerTurn) {
 
+                    //Reads players decision
                     player1Display.printSmall("make a decision...");
                     int i = player1Display.getDecision("Hit", "Stand", "Double", "Split","surrender");
+                    //Option to hit
                     if (i == 1) {
                         Player1Hand.addCard(deck.removeCard());
                     }
+                   //Option to stand
                     if (i == 2) {
                         playerTurn = false;
                     }
+                    //Option to double
                     if(i == 3){
                         betAmount = betAmount * 2;
                         playerTurn = false;
@@ -79,6 +88,7 @@ public class CardRunner
                     if(i == 4){
 
                     }
+                    //Option to surrender
                     if(dec == 5){
                         betAmount = betAmount / 2;
                         surrendered = true;
@@ -93,6 +103,7 @@ public class CardRunner
                 //Dealer goes until done. Hits on soft 17s
                 Dealer.faceUp();
 
+                //Processes turn for the dealer
                 if (!Player1Hand.getBlackJack() && !Player1Hand.getBust()) {
                     while (Dealer.getSum() < 17 || (Dealer.getIsSoft() && Dealer.getSum() == 17)) {
                         player1Display.sleep(1500);
@@ -103,7 +114,7 @@ public class CardRunner
                 }
 
 
-                //decide who wins
+                //Decides who wins
                 if(keepPlaying) {
                     if (Player1Hand.getBust()) {
                         player1Display.printBig("You lose... Bust");
