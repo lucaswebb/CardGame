937eb69e3d7913a145bab4bebe3377648056126b
ekhokhar <ekhokhar@ymail.com>
2014-11-12 11:49:06 -0500

    Comments

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -6,11 +6,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 public class CardDeck
 {
-    private ArrayList<CardCard> deckQue= new ArrayList<CardCard>();
+    private ArrayList<CardCard> deckQue= new ArrayList<CardCard>();//Creates an arraylist to hold a deck of cards
 
     public CardDeck()
     {
-        //add all cards to deck queue
+        //adds all cards (of each suit and number) to deck que
         for(int suite = 0; suite < 4; suite ++){
             for(int number = 1; number <=13; number++){
                         CardCard c = new CardCard(suite, number);
@@ -20,23 +20,24 @@ public class CardDeck
             }
 
 
-//java's shuffle method for lists
+//java's shuffle method for lists, shuffles the deck:
     public void shuffle(){
         Collections.shuffle(deckQue);
     }
-
+//removes a card from the deck, which goes to the user's hand
     public CardCard removeCard(){
         CardCard holder = deckQue.get(0);
         deckQue.remove(0);
         return holder;
     }
+//adds an array of cards to the deck:
     public void addCard(CardCard array[]){
         for(CardCard card : array) {
             deckQue.add(card);
         }
         shuffle();
     }
-
+//converts arraylist to an array for use in other classes:
     public CardCard[] getCards(){
         CardCard deckQue2[] = new CardCard[52];
         for(int index = 0; index < deckQue.size(); index++){
