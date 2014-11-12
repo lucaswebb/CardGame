0c2ad13c797277dae1098340379edc2fa3ca5e27
lucasawebb <lucasawebb@gmail.com>
2014-11-12 11:56:25 -0500

    Fix

--- src/CardRunner.java
+++ src/CardRunner.java
@@ -1,9 +1,8 @@
 /**
  * Created by lucaswebb on 11/3/14.
  */
-public class CardRunner
-{
-    public static void main(String[] args){
+public class CardRunner {
+    public static void main(String[] args) {
 
         //Create a deck and shuffle it
         CardDeck deck = new CardDeck();
@@ -12,7 +11,7 @@ public class CardRunner
         //Create hands and initialize display
         CardHand Player1Hand = new CardHand(false);//input indicates whether the hand is a dealer
         CardHand Dealer = new CardHand(true);
-        CardDisplay player1Display = new CardDisplay(Player1Hand,Dealer, "Player 1");
+        CardDisplay player1Display = new CardDisplay(Player1Hand, Dealer, "Player 1");
 
 
         //Set money and bet amounts and print welcome message
@@ -24,30 +23,30 @@ public class CardRunner
         //Keeps track of whether the player wants to play again
         boolean keepPlaying = true;
         boolean firstTimeThroughLoop = true;
-        while(keepPlaying) {
+        while (keepPlaying) {
             player1Display.printMedium("Your current money is " + money);
             player1Display.printSmall("Would you like to Play?");
 
             //Enable slider and reads players option to play or exit
             player1Display.enableSlider();
             int dec;
-            if(firstTimeThroughLoop)
-                dec = player1Display.getDecision("play", "exit", "", "","");
+            if (firstTimeThroughLoop)
+                dec = player1Display.getDecision("play", "exit", "", "", "");
             else
-                dec = player1Display.getDecision("play again", "exit", "", "","");
+                dec = player1Display.getDecision("play again", "exit", "", "", "");
 
 
             //Checks to make sure bet is not greater than money
-            if(player1Display.getBet() <= money){
+            if (player1Display.getBet() <= money) {
                 betAmount = player1Display.getBet();
             } else {
                 betAmount = money;
                 player1Display.updateBet(betAmount);
             }
-            
+
             deck.addCard(Dealer.removeCards());
             deck.addCard(Player1Hand.removeCards());
-            player1Display.refreshHand(Player1Hand,Dealer);
+            player1Display.refreshHand(Player1Hand, Dealer);
             deck.shuffle();
 
             //Option to exit
@@ -57,14 +56,14 @@ public class CardRunner
                 keepPlaying = false;
 
                 System.out.println("exiting game");
-            //Option to keep playing
-            //Deals more cards and refreshes
+                //Option to keep playing
+                //Deals more cards and refreshes
             } else {
                 Player1Hand.addCard(deck.removeCard());
                 Player1Hand.addCard(deck.removeCard());
                 Dealer.addCard(deck.removeCard());
                 Dealer.addCard(deck.removeCard());
-                player1Display.refreshHand(Player1Hand,Dealer);
+                player1Display.refreshHand(Player1Hand, Dealer);
                 player1Display.printSmall("make a decision");
                 player1Display.printBig("");
 
@@ -76,87 +75,88 @@ public class CardRunner
 
                     //Reads players decision
                     player1Display.printSmall("make a decision...");
-                    int i = player1Display.getDecision("Hit", "Stand", "Double", "Split","surrender");
+                    int i = player1Display.getDecision("Hit", "Stand", "Double", "Split", "surrender");
                     //Option to hit
                     if (i == 1) {
                         Player1Hand.addCard(deck.removeCard());
                     }
-                   //Option to stand
+                    //Option to stand
                     if (i == 2) {
                         playerTurn = false;
                     }
                     //Option to double
-                    if(i == 3){
+                    if (i == 3) {
                         betAmount = betAmount * 2;
                         playerTurn = false;
                         Player1Hand.addCard(deck.removeCard());
                         player1Display.updateBet(betAmount);
                     }
-                    if(i == 4){
+                    if (i == 4) {
 
                     }
-<<<<<<< HEAD
-                    if(i == 5){
 
-=======
-                    //Option to surrender
-                    if(dec == 5){
->>>>>>> FETCH_HEAD
-                        betAmount = betAmount / 2;
-                        surrendered = true;
-                        playerTurn = false;
-                        player1Display.updateBet(betAmount);
-                    }
-                    //leave at the end of loop
-                    player1Display.refreshHand(Player1Hand, Dealer);
-                }
+                    if (i == 5) {
 
 
-                //Dealer goes until done. Hits on soft 17s
-                Dealer.faceUp();
+                        //Option to surrender
+                        if (dec == 5) {
 
-                //Processes turn for the dealer
-                if (!Player1Hand.getBlackJack() && !Player1Hand.getBust()) {
-                    while (Dealer.getSum() < 17 || (Dealer.getIsSoft() && Dealer.getSum() == 17)) {
-                        player1Display.sleep(1500);
-                        Dealer.addCard(deck.removeCard());
-
+                            betAmount = betAmount / 2;
+                            surrendered = true;
+                            playerTurn = false;
+                            player1Display.updateBet(betAmount);
+                        }
+                        //leave at the end of loop
                         player1Display.refreshHand(Player1Hand, Dealer);
                     }
-                }
 
 
-                //Decides who wins
-                if(keepPlaying) {
-                    if (Player1Hand.getBust()) {
-                        player1Display.printBig("You lose... Bust");
-                        money -= betAmount;
-                    } else if (Player1Hand.getBlackJack() && !Dealer.getBlackJack()) {
-                        player1Display.printBig("You win... BlackJack");
-                        money += 2 * betAmount;
-                    }else if (surrendered) {
-                        player1Display.printBig("You surrender");
-                        money -= betAmount;
-                    } else if (Player1Hand.getBlackJack() && Dealer.getBlackJack()) {
-                        player1Display.printBig("Tie: Double BlackJack");
-                    } else if (Dealer.getBust()) {
-                        player1Display.printBig("you win... Dealer busts");
-                        money += betAmount;
-                    } else if (Dealer.getBlackJack()) {
-                        player1Display.printBig("you lost... Dealer gets Blackjack");
-                        money -= 2 * betAmount;
-                    } else if (Dealer.getSum() == Player1Hand.getSum()) {
-                        player1Display.printBig("Tie... both scores: " + Dealer.getSum());
-                    } else if (Dealer.getSum() > Player1Hand.getSum()) {
-                        player1Display.printBig("Dealer wins with " + Dealer.getSum() + " points");
-                        money -= betAmount;
-                    } else if (Dealer.getSum() < Player1Hand.getSum()) {
-                        player1Display.printBig("You win with " + Player1Hand.getSum() + " points");
-                        money += betAmount;
+                    //Dealer goes until done. Hits on soft 17s
+                    Dealer.faceUp();
+
+                    //Processes turn for the dealer
+                    if (!Player1Hand.getBlackJack() && !Player1Hand.getBust()) {
+                        while (Dealer.getSum() < 17 || (Dealer.getIsSoft() && Dealer.getSum() == 17)) {
+                            player1Display.sleep(1500);
+                            Dealer.addCard(deck.removeCard());
+
+                            player1Display.refreshHand(Player1Hand, Dealer);
+                        }
+                    }
+
+
+                    //Decides who wins
+                    if (keepPlaying) {
+                        if (Player1Hand.getBust()) {
+                            player1Display.printBig("You lose... Bust");
+                            money -= betAmount;
+                        } else if (Player1Hand.getBlackJack() && !Dealer.getBlackJack()) {
+                            player1Display.printBig("You win... BlackJack");
+                            money += 2 * betAmount;
+                        } else if (surrendered) {
+                            player1Display.printBig("You surrender");
+                            money -= betAmount;
+                        } else if (Player1Hand.getBlackJack() && Dealer.getBlackJack()) {
+                            player1Display.printBig("Tie: Double BlackJack");
+                        } else if (Dealer.getBust()) {
+                            player1Display.printBig("you win... Dealer busts");
+                            money += betAmount;
+                        } else if (Dealer.getBlackJack()) {
+                            player1Display.printBig("you lost... Dealer gets Blackjack");
+                            money -= 2 * betAmount;
+                        } else if (Dealer.getSum() == Player1Hand.getSum()) {
+                            player1Display.printBig("Tie... both scores: " + Dealer.getSum());
+                        } else if (Dealer.getSum() > Player1Hand.getSum()) {
+                            player1Display.printBig("Dealer wins with " + Dealer.getSum() + " points");
+                            money -= betAmount;
+                        } else if (Dealer.getSum() < Player1Hand.getSum()) {
+                            player1Display.printBig("You win with " + Player1Hand.getSum() + " points");
+                            money += betAmount;
+                        }
                     }
                 }
+                firstTimeThroughLoop = false;
             }
-            firstTimeThroughLoop = false;
         }
     }
 }
